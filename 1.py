580debfc7bc155c62b88b71ffe0955923406869f
MrBWorld24 <balagopal24x7@gmail.com>
2025-03-07 11:34:07 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+age=int(input("ENTER THE AGE OF THE USER : "))
+if(age>=18 and age<=66):
+    print("YOU CAN ENTER THE WEBSITE ! !")
+elif(age>66):
+    print("OLD!!!")
+    print("YOU ARE NOT ELIGIBLE TO ENTER THE WEBSITE ! ! ! ! ")
+else:
+    print("YOUNG")
+    print("YOU ARE NOT ELIGIBLE TO ENTER THE WEBSITE ! ! ! ! ")
+print("hai guyzzz!!!!!!!")
